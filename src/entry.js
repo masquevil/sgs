25215d2921953f8c8e997cf02773ac145839a682
masquevil <masquevil@gmail.com>
2017-03-10 13:41:01 +0800

    lazy shoudong

--- src/entry.js
+++ src/entry.js
@@ -9,7 +9,8 @@ var countries = {
 imgs = imgs.map(function(name){
     return {
         key: countries[name.slice(5, 6)] + name.slice(6, 8) + name.slice(9),
-        url: name
+        file: name,
+        url: ''
     };
 }).sort(function(a, b){
     return a.key < b.key ? -1 : 1;
@@ -25,10 +26,17 @@ window.page = new Vue({
     },
     computed: {
         result: function(){
-            var vm = this;
-            return imgs.filter(function(img){
+            var vm = this, result;
+            result = imgs.filter(function(img){
                 return !!~img.key.indexOf(vm.keyword.toLowerCase()) && img.key.indexOf(vm.country) === 0;
             });
+            result[0].url = result[0].file;
+            return result;
+        }
+    },
+    methods: {
+        showImg: function(index){
+            this.result[index].url = this.result[index].file;
         }
     }
 });
